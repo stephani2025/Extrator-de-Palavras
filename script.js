c1a69f4fe5a510a3ddabcb2150e788b9607d0cb2
Stephani <stephani.vitoria.ramos@escola.pr.gov.br>
2025-09-16 09:32:38 -0300

    Aula 4

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
 function mostraPalavrasChave() {
   const texto = document.querySelector("#entrada-de-texto").value;
-
   const campoResultado = document.querySelector("#resultado-palavrachave");
+  const palavrasChave = processaTexto(texto);
 
-  const palavras = texto.split(" ");
+  campoResultado.textContent = palavras.join(", ");
+}
 
-  campoResultado.textContent = palavras;
+function processaTexto(texto) {
+  let palavras = texto.split(/\P{L}+/u);
+  return palavras;
 }
