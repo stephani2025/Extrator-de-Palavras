fac7d868f45d89f90d2a6b5e1af695c5a9fe5ae6
Stephani <stephani.vitoria.ramos@escola.pr.gov.br>
2025-09-30 09:38:04 -0300

    .

--- script.js
+++ script.js
@@ -12,5 +12,26 @@ function mostraPalavrasChave() {
 
 function processaTexto(texto) {
   let palavras = texto.split(/\P{L}+/u);
-  return palavras;
+  const frequencias = contafrequencias(palavras);
+  let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+  function ondenaPalavra(p1, p2) {
+      return frequencias[p2] - frequencias[p1];
+  }
+
+  console.log(ordenadas);
+  return ordenadas.slice(0,10);
+}
+
+function contaFrequencias(palavras) {
+  let frequencias = {};
+  for (let i of palavras) {
+    frequencias[i] = 0;
+    for (let j of palavras) {
+      if (i == j) {
+        frequencias[1]++;
+      }
+    }
+  }
+  return frequencias;
 }
